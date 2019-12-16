48ed42b6abb9c72c4419d667a666a03d39a82d82
TonyV <tvester1487@gmail.com>
2019-12-15 22:02:08 -0600

    Complete Submission

--- employeetracker.js
+++ employeetracker.js
@@ -29,14 +29,12 @@ function runSearch() {
       message: "What would you like to do?",
       choices: [
         "View all employees",
-        "View all employees by Department",
-        "View all employees by Manager",
         "Add employee",
-        "Remove employee",
         "Update employee role",
-        "Update employee manager",
         "View roles",
-        "View managers",
+        "Add role",
+        "View departments",
+        "Add department",
         "exit"
       ]
     })
@@ -46,25 +44,29 @@ function runSearch() {
           allEmployees();
           break;
 
-        case "View all employees by Department":
-          employeeByDepartment();
+        case "Add employee":
+          addEmployee();
           break;
 
-        case "View all employees by Manager":
-          employeeByManager();
+        case "Update employee role":
+          updateEmployeeRole();
           break;
 
-        // case "Add a role":
-        //   addRole();
-        //   break;
+        case "View roles":
+          viewRoles();
+          break;
 
-        //   case "View employees":
-        //     viewRoles();
-        //     break;
+        case "Add role":
+          addRole();
+          break;
 
-        //   case "Add an employee":
-        //     addRole();
-        //     break;
+        case "View departments":
+          viewDepartments();
+          break;
+
+        case "Add department":
+          addDepartment();
+          break;
 
         case "exit":
           connection.end();
@@ -73,12 +75,268 @@ function runSearch() {
     });
 }
 
-// Add departments, roles, employees
+// View all employees
+function allEmployees() {
+  var query = "SELECT * FROM employee";
+  connection.query(query, (err, res) => {
+    if (err) throw err;
+    for (var i = 0; i < res.length; i++) {
+      console.log(`Name: ${res[i].first_name} ${res[i].last_name}`);
+    }
+    runSearch();
+  });
+}
 
-// View departments, roles, employees
-// function viewCompany(){
-//     inquirer.pro
-// }
+// View roles
+function viewRoles() {
+  var query = "SELECT * FROM role";
+  connection.query(query, (err, res) => {
+    if (err) throw err;
+    for (var i = 0; i < res.length; i++) {
+      console.log(`Role: ${res[i].title}  ||  Salary: $${res[i].salary}`);
+    }
+    runSearch();
+  });
+}
+
+// View departments
+function viewDepartments() {
+  var query = "SELECT * FROM department";
+  connection.query(query, (err, res) => {
+    if (err) throw err;
+    for (var i = 0; i < res.length; i++) {
+      console.log(`Department: ${res[i].dept_name}`);
+    }
+    runSearch();
+  });
+}
+
+// Add employee
+function addEmployee() {
+  inquirer
+    .prompt([
+      {
+        name: "first_name",
+        type: "input",
+        message: "What is the employee's first name?"
+      },
+      {
+        name: "last_name",
+        type: "input",
+        message: "What is the employee's last name?"
+      },
+      {
+        name: "role",
+        type: "list",
+        choices: [
+          "Sales Manager",
+          "Sales Associate",
+          "Accounts Payable",
+          "Accounts Receivable"
+        ],
+        message: "What is the employee's role?"
+      }
+    ])
+    .then(function(answer) {
+      var roleId = null;
+      var managerId = null;
+
+      if ((answer.role = "Sales Manager")) {
+        roleId = 4;
+        managerId = 3;
+      }
+      if ((answer.role = "Sales Associate")) {
+        roleId = 5;
+        managerId = 3;
+      }
+      if ((answer.role = "Accounts Payable")) {
+        roleId = 10;
+        managerId = 11;
+      }
+      if ((answer.role = "Accounts Receivable")) {
+        roleId = 11;
+        managerId = 11;
+      }
+
+      connection.query(
+        "INSERT INTO employee SET ?",
+
+        {
+          first_name: answer.first_name,
+          last_name: answer.last_name,
+          role_id: roleId,
+          manager_id: managerId
+        },
+
+        function(err) {
+          if (err) throw err;
+          console.log("Employee added successfully!");
+          runSearch();
+        }
+      );
+    });
+}
+
+// Add role
+function addRole() {
+  inquirer
+    .prompt([
+      {
+        name: "title",
+        type: "input",
+        message: "What is the title of the new role?"
+      },
+      {
+        name: "salary",
+        type: "input",
+        message: "What is the salary of the new role?"
+      },
+      {
+        name: "dept",
+        type: "list",
+        choices: ["Executive", "Sales", "Marketing", "Finance"],
+        message: "What department will the new role be in?"
+      }
+    ])
+    .then(function(answer) {
+      dept_id = null;
+
+      if ((answer.dept = "Executive")) {
+        dept_id = 1;
+      }
+      if ((answer.dept = "Sales")) {
+        dept_id = 2;
+      }
+      if ((answer.dept = "Marketing")) {
+        dept_id = 3;
+      }
+      if ((answer.dept = "Finance")) {
+        dept_id = 4;
+      }
+
+      connection.query(
+        "INSERT INTO role SET ?",
+        {
+          title: answer.title,
+          salary: answer.salary,
+          department_id: dept_id
+        },
+        function(err) {
+          if (err) throw err;
+          console.log("Role added successfully!");
+          runSearch();
+        }
+      );
+    });
+}
+
+// Add department
+function addDepartment() {
+  inquirer
+    .prompt([
+      {
+        name: "name",
+        type: "input",
+        message: "What is the name of the new department?"
+      }
+    ])
+    .then(function(answer) {
+      connection.query(
+        "INSERT INTO department SET ?",
+        {
+          dept_name: answer.name
+        },
+        function(err) {
+          if (err) throw err;
+          console.log("Department added successfully!");
+          runSearch();
+        }
+      );
+    });
+}
+
+// Update employee roles
+function updateEmployeeRole() {
+  connection.query("SELECT * FROM employee", function(err, results) {
+    if (err) throw err;
+
+    inquirer
+      .prompt([
+        {
+          name: "employee",
+          type: "rawlist",
+          choices: function() {
+            var choiceArray = [];
+            for (var i = 0; i < results.length; i++) {
+              choiceArray.push(results[i].first_name + results[i].last_name);
+            }
+            return choiceArray;
+          },
+          message: "Which employee's role would you like to update?"
+        },
+        {
+          name: "role",
+          type: "list",
+          choices: [
+            "Sales Manager",
+            "Sales Associate",
+            "Accounts Payable",
+            "Accounts Receivable"
+          ],
+          message: "What new role would you like to assign?"
+        }
+      ])
+      .then(function(answer) {
+        var roleId = null;
+        var managerId = null;
+
+        if ((answer.role = "Sales Manager")) {
+          roleId = 4;
+          managerId = 3;
+        }
+        if ((answer.role = "Sales Associate")) {
+          roleId = 5;
+          managerId = 3;
+        }
+        if ((answer.role = "Accounts Payable")) {
+          roleId = 10;
+          managerId = 11;
+        }
+        if ((answer.role = "Accounts Receivable")) {
+          roleId = 11;
+          managerId = 11;
+        }
+
+        var chosenItem;
+        for (var i = 0; i < results.length; i++) {
+          if (
+            results[i].first_name + results[i].last_name ===
+            answer.employee
+          ) {
+            chosenItem = results[i];
+          }
+        }
+
+        connection.query(
+          "UPDATE employee SET ? WHERE ?",
+          [
+            {
+              role_id: roleId,
+              manager_id: managerId
+            },
+            {
+              id: chosenItem.id
+            }
+          ],
+          function(err) {
+            if (err) throw err;
+            console.log("Employee role succsesfully changed!");
+            runSearch();
+          }
+        );
+      });
+  });
+}
 
 // function songSearch() {
 //     inquirer
